b9cc45f91fce9dd57ec2b7463425f4744c2b7dfb
a.volkov.linux <alex@a.volko.t>
2017-01-27 13:47:42 +0300

    Add style and new functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 console.log('js');
 $('#datetimepicker1').datepicker();
+$('#datetimepicker2').datepicker();
+
 function check()
 {
     var inp = $('[name=optradio]');
     for (var i = 0; i < inp.length; i++) {
         if (inp[i].type == "radio" && inp[i].checked) {
             if(inp[i].value === 'messageId') {
-                $('fast-message__messageId').addClass('show-message-id');
+                $('.fast-message__messageId').addClass('show-message-id');
+                $('.fast-message__creatMessage').removeClass('show-creat-message');
             }
             if(inp[i].value === 'creatMessage') {
-                document.getElementsByClassName('fast-message__messageId').classList = 'show-creat-message';
+                $('.fast-message__creatMessage').addClass('show-creat-message');
+                $('.fast-message__messageId').removeClass('show-message-id');
             }
         }
     }

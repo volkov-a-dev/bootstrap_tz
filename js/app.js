c558acf64aea30452285032f042cf7e285cc628f
a.volkov.linux <alex@a.volko.t>
2017-01-27 03:08:31 +0300

    Creat new style

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 console.log('js');
 $('#datetimepicker1').datepicker();
+function check()
+{
+    var inp = $('[name=optradio]');
+    for (var i = 0; i < inp.length; i++) {
+        if (inp[i].type == "radio" && inp[i].checked) {
+            if(inp[i].value === 'messageId') {
+                $('fast-message__messageId').addClass('show-message-id');
+            }
+            if(inp[i].value === 'creatMessage') {
+                document.getElementsByClassName('fast-message__messageId').classList = 'show-creat-message';
+            }
+        }
+    }
+}
